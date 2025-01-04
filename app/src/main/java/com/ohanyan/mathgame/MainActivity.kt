06bdbdb8a6d3a530657e2a6d734e9f219ee8e7e5
gorohan <gorwork21@gmail.com>
2025-01-04 17:40:17 +0400

    commit

--- app/src/main/java/com/ohanyan/mathgame/MainActivity.kt
+++ app/src/main/java/com/ohanyan/mathgame/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MathTheme {
+
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
