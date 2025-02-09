ca70ebaf5f487ebef48c179aa6ab37a25e86c55d
gorohan <gorwork21@gmail.com>
2025-02-09 23:58:27 +0400

    add lottie animation to feature plugin.

--- app/src/main/java/com/ohanyan/mathgame/MainActivity.kt
+++ app/src/main/java/com/ohanyan/mathgame/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.ohanyan.mathgame.designsystem.theme.MathAppTheme
 import com.ohanyan.mathgame.onboarding.splash.SplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
--- feature/onboarding/src/main/java/com/ohanyan/mathgame/onboarding/splash/SplashScreen.kt
+++ feature/onboarding/src/main/java/com/ohanyan/mathgame/onboarding/splash/SplashScreen.kt
@@ -1,16 +1,38 @@
 package com.ohanyan.mathgame.onboarding.splash
 
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.ohanyan.mathgame.designsystem.component.header.HeaderFromTop
-import com.ohanyan.mathgame.designsystem.theme.MathAppTheme
+import com.ohanyan.mathgame.onboarding.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun SplashScreen(
@@ -22,13 +44,83 @@ fun SplashScreen(
 
 @Composable
 fun SplashScreenUI(uiState: SplashUIState) {
+
+    var offsetX by remember { mutableStateOf(0.dp) }
+    var offsetY by remember { mutableStateOf(0.dp) }
+
+    val animatedOffsetX by animateDpAsState(
+        targetValue = offsetX,
+        animationSpec = tween(durationMillis = 2000), label = "" // Animation duration for each step
+    )
+    val animatedOffsetY by animateDpAsState(
+        targetValue = offsetY,
+        animationSpec = tween(durationMillis = 2000), label = "" // Animation duration for each step
+    )
+
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            repeat(1) { step ->
+                offsetX += 300.dp //
+                offsetY -= 90.dp
+                delay(1400) // Wait for the animation to complete
+            }
+        }
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = MathAppTheme.colors.coreBlue)
     ) {
+        Image(
+            painter = painterResource(R.drawable.backtest2),
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = null,
+        )
         HeaderFromTop(uiState.title)
+
+        AnimatedPreloader(
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .align(Alignment.BottomStart)
+                .offset(
+                    x = animatedOffsetX,
+                    y = animatedOffsetY
+                )
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+
+                        offsetX += dragAmount.x.toDp()
+                        offsetY += dragAmount.y.toDp()
+                    }
+                }
+        )
     }
+}
+
+@Composable
+fun AnimatedPreloader(modifier: Modifier = Modifier) {
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.main_hero
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+
+    LottieAnimation(
+        composition = preloaderLottieComposition,
+        progress = { preloaderProgress },
+        modifier = modifier
+    )
 }
 
 @Preview
