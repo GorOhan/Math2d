84448effa098215f6dd83cb551b299043076e213
gorohan <gorwork21@gmail.com>
2025-02-09 13:16:52 +0400

    gradle files clean up .

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -8,14 +8,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     api(libs.androidx.compose.foundation)
-
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.material3.adaptive)
     api(libs.androidx.compose.material3.navigationSuite)
