02701b686b4c0ad1ad15842d7e5a0657b33ce752
gorohanyan <gorohanyan@bethinklabs.com>
2025-07-25 20:09:32 +0400

    working on settings screen.

--- feature/settings/src/main/java/com/ohanyan/mathgame/settings/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/com/ohanyan/mathgame/settings/settings/SettingsScreen.kt
@@ -1,22 +1,35 @@
 package com.ohanyan.mathgame.settings.settings
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchColors
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ohanyan.mathgame.designsystem.preview.MathPreview
@@ -30,21 +43,33 @@ fun SettingsScreen(
     viewModel: SettingsViewModel = hiltViewModel(),
     onBackClick: () -> Unit = {}
 ) {
-
+    var showSelectLanguage by remember { mutableStateOf(false) }
     val uiState by viewModel.uiState.collectAsState()
 
     SettingsScreenUI(
         settingScreenUIState = uiState,
         onMusicOnChane = viewModel::onMusicOnChange,
-        onBackClick = onBackClick
+        onBackClick = onBackClick,
+        onSelectLanguageClick = {
+            showSelectLanguage = true
+        }
     )
+
+    if (showSelectLanguage) {
+        SelectLanguageAlert(
+            onDismissRequest = {
+                showSelectLanguage = false
+            }
+        )
+    }
 }
 
 @Composable
 fun SettingsScreenUI(
     settingScreenUIState: SettingScreenUIState = SettingScreenUIState(),
     onMusicOnChane: (Boolean) -> Unit = {},
-    onBackClick: () -> Unit = {}
+    onBackClick: () -> Unit = {},
+    onSelectLanguageClick: () -> Unit = {},
 ) {
     Box(
         modifier = Modifier
@@ -73,7 +98,8 @@ fun SettingsScreenUI(
         MainContent(
             modifier = Modifier.align(Alignment.Center),
             settingScreenUIState = settingScreenUIState,
-            onMusicOnChane = onMusicOnChane
+            onMusicOnChane = onMusicOnChane,
+            onSelectLanguageClick = onSelectLanguageClick
         )
 
     }
@@ -90,7 +116,9 @@ fun MainContent(
     modifier: Modifier,
     settingScreenUIState: SettingScreenUIState,
     onMusicOnChane: (Boolean) -> Unit,
-) {
+    onSelectLanguageClick: () -> Unit,
+
+    ) {
     Column(modifier = modifier) {
         Row(
             verticalAlignment = Alignment.CenterVertically
@@ -110,6 +138,95 @@ fun MainContent(
                     checkedThumbColor = MathAppTheme.colors.coreBlue,
                     checkedTrackColor = MathAppTheme.colors.coreYellow
                 )
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.padding(end = 24.dp),
+                text = "Select Language",
+                style = MathAppTheme.typography.h1Bee,
+                color = MathAppTheme.colors.red
+            )
+
+            Image(
+                modifier = Modifier
+                    .size(42.dp)
+                    .clip(RoundedCornerShape(80f))
+                    .clickable {
+                        onSelectLanguageClick()
+                    },
+                painter = painterResource(com.ohanyan.mathgame.ui.R.drawable.flag_am),
+                contentDescription = "select language icon"
+            )
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SelectLanguageAlert(
+    onDismissRequest: () -> Unit = {}
+) {
+    BasicAlertDialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(.5f)
+                .clip(
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .border(
+                    border = BorderStroke(
+                        2.dp, color = MathAppTheme.colors.blue30,
+                    ),
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            MathAppTheme.colors.coreBlue.copy(0.91f),
+                            MathAppTheme.colors.coreBlue.copy(1f)
+                        )
+                    )
+                )
+                .padding(horizontal = 24.dp, vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(16.dp))
+                    .size(48.dp)
+                    .border(
+                        border = BorderStroke(
+                            4.dp, color = MathAppTheme.colors.coreYellow,
+                        ),
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                contentDescription = "en",
+                contentScale = ContentScale.FillBounds,
+                painter = painterResource(com.ohanyan.mathgame.ui.R.drawable.flag_gb)
+            )
+            Image(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(16.dp))
+                    .size(48.dp),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = "ru",
+                painter = painterResource(com.ohanyan.mathgame.ui.R.drawable.flag_ru)
+            )
+            Image(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(16.dp))
+                    .size(48.dp),
+                contentScale = ContentScale.FillBounds,
+                contentDescription = "eng",
+                painter = painterResource(com.ohanyan.mathgame.ui.R.drawable.flag_am)
             )
         }
     }
