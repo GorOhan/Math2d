3363aa71f219c3afd566cd92b1fdad58e7cb297c
gorohan <gorwork21@gmail.com>
2026-03-22 11:42:39 +0400

    release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 
     defaultConfig {
         applicationId = "com.ohanyan.mathgame"
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
